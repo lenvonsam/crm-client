e5f9664025ef550f41da587386c93972c023dc5c
sam <sam@thinkingsam.cn>
2019-10-28 09:23:29 +0800

    change ious xy proxy to dev

--- server/proxy.js
+++ server/proxy.js
@@ -9,7 +9,7 @@ const hptl = require('../utils/httpUtil')
 // const proxyUrl = 'http://localhost:7786/'
 // const proxyUrl = 'http://192.168.80.91:8080/crmserver/'
 const proxyUrl = 'http://192.168.80.200:8080/crmserver/'
-const xyUrl = 'http://172.16.120.248/gzql'
+const xyUrl = 'http://192.168.80.249/gzql'
 
 router.use((req, res, next) => {
   Object.setPrototypeOf(req, app.request)
