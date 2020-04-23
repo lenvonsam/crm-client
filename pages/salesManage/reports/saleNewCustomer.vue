1141c87f4a04eee694c7306a4f4b7d2e098f0fbc
zhjm <943257156@qq.com>
2020-04-23 08:05:29 +0800

    '销售专员新增客户明细表增加导出'

--- pages/salesManage/reports/saleNewCustomer.vue
+++ pages/salesManage/reports/saleNewCustomer.vue
@@ -1,6 +1,8 @@
   <template lang="pug">
   .content
     breadcrumb(:breadItems="breadItems")
+    .pt-15(v-if="currentUser.loginAcct === 'zq' || currentUser.loginAcct === 'admin'")
+      button-group(:btns="btnGroups", @groupBtnClick="groupBtnClick")
     .mt-15
       search-form(:searchFormItems="searchFormItems", @search="searchForm")
     .pt-15
@@ -11,17 +13,19 @@
 import breadcrumb from '@/components/Breadcrumb.vue'
 import basicTable from '@/components/BasicTable.vue'
 import searchForm from '@/components/SearchForm.vue'
-// import buttonGroup from '@/components/ButtonGroup.vue'
+import buttonGroup from '@/components/ButtonGroup.vue'
 import { mapState } from 'vuex'
 export default {
   layout: 'main',
   components: {
     breadcrumb,
     basicTable,
-    searchForm
+    searchForm,
+    buttonGroup
   },
   data () {
     return {
+      btnGroups: [{ lbl: '导出', type: 'export' }],
       breadItems: ['销售管理', '销售专员新增客户明细表'],
       currentPage: 1,
       totalCount: 0,
@@ -146,6 +150,38 @@ export default {
     }
   },
   methods: {
+    groupBtnClick () {
+      this.pageShow(this)
+      this.exportExcel()
+    },
+    async exportExcel () {
+      try {
+        const tempHead = []
+        this.tableValue.tableHead.map(item => {
+          tempHead.push(item.lbl)
+        })
+        const searchFrom = this.$refs.searchFrom.getSearchParm('submit')
+        const params = {
+          nyStart: searchFrom.nyStart,
+          nyEnd: searchFrom.nyEnd,
+          customer: searchFrom.customer,
+          dptName: searchFrom.dptName,
+          empCode: searchFrom.empCode,
+          nameType: 0,
+          uid: this.currentUser.id
+        }
+        const { data } = await this.apiStreamPost('/proxy/common/post', { url: 'erpReport/export/saleNewCustomer', params: params })
+        if (!data.list) {
+          this.msgShow('导出失败')
+          return false
+        }
+        this.excelExport(tempHead, data.list, this.breadItems[1])
+        this.pageHide(this)
+      } catch (e) {
+        this.pageHide(this)
+        console.error(e)
+      }
+    },
     async loadData () {
       try {
         let url = `erpReport/saleNewCustomer?pageSize=${this.pageSize}&currentPage=${this.currentPage - 1}&uid=${this.currentUser.id}&nameType=0`
@@ -173,9 +209,6 @@ export default {
           if (paramsObj[key]) {
             searchValList.push(`${key}=${paramsObj[key]}`)
           }
-          //  else if (this.searchRule[key]) {
-          //   throw new Error(this.searchRule[key])
-          // }
         })
         this.currentPage = 1
         this.searchValStr = searchValList.toString().replace(/,/g, '&')
